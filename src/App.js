1888dfc588408d1483e1a33b0ffe624d825a5d22
babarAli <rana.babar@showday.io>
2022-09-05 12:57:12 +0500

    my commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const showNotification = () => {
+    // create a new notification
+    console.log("1");
+    const notification = new Notification("Innroad Guest Message", {
+      body: "on res cre 22807778. If you do not wish to receive messages, you can opt-out at any time by replying “STOP”. Please note, message and data rates may apply.",
+      icon: logo,
+      vibrate: true,
+    });
+    console.log("2", notification);
+
+    // close the notification after 10 seconds
+    setTimeout(() => {
+      notification.close();
+    }, 10 * 1000);
+
+    console.log("3");
+    // navigate to a URL
+    notification.addEventListener("click", () => {
+      window.open("https://www.qainnroad.com/", "_blank");
+    });
+    console.log("4");
+  };
+
+  setInterval(() => showNotification(), 7000);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        <div
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          onClick={showNotification}
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Show Notification
+        </div>
+        or you will get notifications after every 7 seconds
       </header>
     </div>
   );
